7fe174d7c4428bf6188ace7b85ab25e5c77ca010
Mitch Stroebel <mitch.sixty6@gmail.com>
2024-12-18 11:24:29 +0200

    Added toast messages, updated routes

--- assets/app.js
+++ assets/app.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
 
-import './styles/app.css';
+import "./styles/app.css";
+import "./styles/mobile.css";
 
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
-import Admin from './pages/Admin';
-import ClassRoom from './pages/ClassRoom';
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Admin from "./pages/Admin";
+import ClassRoom from "./pages/ClassRoom";
+import Page404 from "./pages/Page404";
+import Page500 from "./pages/Page500";
 
-import Login from './forms/Login';
-import UserUpdate from './forms/UserUpdate';
-import UserCreate from './forms/UserCreate';
+import Login from "./forms/Login";
+import UserUpdate from "./forms/UserUpdate";
+import UserCreate from "./forms/UserCreate";
 
-import ClassRoomUpdate from './forms/ClassRoomUpdate';
-import ClassRoomCreate from './forms/ClassRoomCreate';
-    
+import ClassRoomUpdate from "./forms/ClassRoomUpdate";
+import ClassRoomCreate from "./forms/ClassRoomCreate";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
@@ -25,13 +29,17 @@ root.render(
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/admin" element={<Admin />} />
       <Route path="/classroom/:classRoom" element={<ClassRoom />} />
-
       <Route path="/login" element={<Login />} />
       <Route path="/user/new" element={<UserCreate />} />
       <Route path="/user/:user/update" element={<UserUpdate />} />
-
       <Route path="/classroom/new" element={<ClassRoomCreate />} />
-      <Route path="/classroom/:classRoom/update" element={<ClassRoomUpdate />} />
+      <Route path="/error" element={<Page500 />} />
+      <Route
+        path="/classroom/:classRoom/update"
+        element={<ClassRoomUpdate />}
+      />
+      <Route path="*" element={<Page404 />} />
     </Routes>
+    <Toaster />
   </BrowserRouter>
 );
